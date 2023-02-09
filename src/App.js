b9762f900fa92f19f63a40011955239f83c6f039
Abhishek Sukumar <abhishekskmr44@gmail.com>
2023-02-09 11:36:59 +0530

    git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Search from './search/search';
+import CurrentWeather from './components/current-weather/current-weather';
+import {WEATHER_API_URL,WEATHER_API_KEY} from "./api";
+
 
 function App() {
+
+const [currentWeather, setCurrentWeather] = 
+
+
+
+
+const handleOnSearchChange =(searchData) => {
+const [lat,lon] = searchData.value.split(" ");
+
+const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?${lat}={lat}&${lon}={lon}&appid=${WEATHER_API_KEY}`) 
+
+
+
+const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?${lat}={lat}&${lon}={lon}&appid=${WEATHER_API_KEY}`) 
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+Promise.all([currentWeatherFetch, forecastFetch])
+.then(async(response)=>{
+  const weatherResponse = await  response[0].json();
+
+  const forcastResponse = await  response[1].json();
+
+})
+
+
+} 
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    
+    <Search onSearchChange={handleOnSearchChange}/>
+    <CurrentWeather/>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,16 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b79f73c914mshd23be6f582e9cfep1a0a19jsnb75e1940d5f4',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+
+export const WEATHER_API_KEY = "69033cff89cd0eb9d49391143ade29ad"
